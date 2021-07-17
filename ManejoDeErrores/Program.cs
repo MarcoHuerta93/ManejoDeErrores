0ad9fe750c7383fad8d14cdd2dcd63c001fb5391
Marco Huerta <marco.huerta.sc@pm.me>
2021-07-17 17:16:08 -0500

    TryCatch

---
+++ ManejoDeErrores/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ManejoDeErrores
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresar un número");
+            string valorIngresado = Console.ReadLine();
+
+            int valorComoInt = int.Parse(valorIngresado);
+            Console.Read();
+        }
+    }
+}
