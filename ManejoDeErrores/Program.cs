198c3425633f91f1ce39b092dddc8334bc86c3f7
Marco Huerta <marco.huerta.sc@pm.me>
2021-07-17 17:27:03 -0500

    TRY CATH FINALLY

--- ManejoDeErrores/Program.cs
+++ ManejoDeErrores/Program.cs
@@ -9,7 +9,25 @@ namespace ManejoDeErrores
             Console.WriteLine("Ingresar un número");
             string valorIngresado = Console.ReadLine();
 
-            int valorComoInt = int.Parse(valorIngresado);
+            try
+            {
+                int valorComoInt = int.Parse(valorIngresado);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("El formato del valor es incorrecto");
+            }
+            catch(OverflowException)
+            {
+                Console.WriteLine("El número ingresado es demasiado largo o muy corto");
+            }
+            catch(ArgumentNullException)
+            {
+                Console.WriteLine("No se ha ingresado ningún número");
+            }
+            finally { Console.WriteLine("Esta frase aparecerá pase lo que pase"); }
+
+           
             Console.Read();
         }
     }
